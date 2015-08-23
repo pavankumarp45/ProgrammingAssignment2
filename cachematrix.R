a44a51ee03df569b61f6b59762249b6e785032f6
pavankumarp45 <pavankumarp45@gmail.com>
2015-08-23 14:13:26 +0530

    R programming Assignment 2
    My name is K PAVAN KUMAR, my course id is "rprog-31"

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## when Matrix is provided as arguement to "makeCacheMatrix" function,
+## it outputs list containing functions set,get,setinverse,getinverse
+## The use of <<- operator is that it assigns new environment name 
+## when each time you call this function and there are some changes in values of 
+## variable "x". Like that "i" is also given same environment name as that of "x"
+## 
 
-## Write a short comment describing this function
-
+## "makeCacheMatrix" creates matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+  i<-NULL
+  set<-function(y){
+    x<<-y
+    i<<-NULL
+  }
+  get<-function() x
+  setinverse<-function(inverse) i<<-inverse
+  getinverse<-function() i
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
+## When the returned value from "makeCacheMatrix" is provided as arguement
+## "CacheSolve" function, it checks whether the inverse of matrix is "x" is
+## calculated or not. If yes, it returns the cached result. If not it solves 
+## the inverse of the matrix and the value is assigned to x$setinverse of that
+## environment name. So that when same list is provided as arguement to
+## "CacheSolve" function, it checks whether that arguement environment name is
+## same as that of cached value variable environment name. If yes, it returns the 
+## cached value.
+##
 
-## Write a short comment describing this function
+## Calculates Inverse of Matrix if it is new, Otherwise returns Cache
 
 cacheSolve <- function(x, ...) {
+  i<-x$getinverse()
+  if(!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  data<-x$get()
+  i<-solve(data,...)
+  x$setinverse(i)
+  i
         ## Return a matrix that is the inverse of 'x'
 }
